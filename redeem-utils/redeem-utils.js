122c75132643f72e1d4f474a0ed50c7fd181a839
robinlinus <robinlinus@users.noreply.github.com>
2018-01-31 16:17:51 -0600

    Add redeem-utils

---
+++ redeem-utils/redeem-utils.js
@@ -0,0 +1,17 @@
+export default class RedeemTools {
+
+    async fetchBalance(address) {
+        const wallet = await Nimiq.Wallet.generate();
+        console.log('Nimiq address: ' + wallet.address.toUserFriendlyAddress());
+        const ethAddress = await RedeemTools.nim2ethAddress(wallet.address);
+        console.log('Ethereum address: ' + ethAddress);
+
+        await response = fetch(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xcfb98637bcae43C13323EAa1731cED2B716962fD&tag=latest&address=${ethAddress}`)
+        return response.json();
+    }
+
+    async nim2ethAddress(address) {
+        const hash = await Nimiq.Hash.sha256(address.serialize());
+        return '0x' + Nimiq.BufferUtils.toHex(hash.subarray(0, 20));
+    }
+}
