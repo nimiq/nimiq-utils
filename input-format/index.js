c1850db6089808b0bb8cf5e45fa7292d5b30c476
Don Maxito <mh@expertsinside.com>
2018-01-31 16:49:48 -0600

    add input-format

---
+++ input-format/index.js
@@ -0,0 +1,18 @@
+export { default as templateParser } from './source/template parser.js'
+
+export { default as templateFormatter } from './source/template formatter.js'
+
+export { default as parseDigit } from './source/parse digit.js'
+
+export { default as parse } from './source/parse.js'
+
+export { default as format } from './source/format.js'
+
+export
+{
+	onChange,
+	onPaste,
+	onCut,
+	onKeyDown
+}
+from './source/input control.js'
