c8d401edf09d92540e24b817dcda22595ccbbae1
Don Maxito <mh@expertsinside.com>
2018-02-05 21:31:49 -0600

    activation-utils: activateAddress

--- activation-utils/activation-utils.js
+++ activation-utils/activation-utils.js
@@ -1,17 +1,12 @@
 export default class ActivationUtils {
     static get API_ROOT() { return 'https://activate.nimiq-network.com' }
 
-    async fetchBalance(address) {
-        const wallet = await Nimiq.Wallet.generate();
-        console.log('Nimiq address: ' + wallet.address.toUserFriendlyAddress());
-        const ethAddress = await RedeemTools.nim2ethAddress(wallet.address);
-        console.log('Ethereum address: ' + ethAddress);
-
+    static async fetchBalance(ethAddress) {
         const response = await fetch(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xcfb98637bcae43C13323EAa1731cED2B716962fD&tag=latest&address=${ethAddress}`)
-        return response.json();
+        return await response.json();
     }
 
-    async nim2ethAddress(address) {
+    static async nim2ethAddress(address) {
         const hash = await Nimiq.Hash.sha256(address.serialize());
         return '0x' + Nimiq.BufferUtils.toHex(hash.subarray(0, 20));
     }
@@ -19,10 +14,7 @@ export default class ActivationUtils {
     async getDashboardData(dashboardToken) {
         const request = fetch(
             `${ActivationUtils.API_ROOT}/list/${dashboardToken}`,
-            {
-                method: 'GET',
-                headers: new Headers()
-            }
+            { method: 'GET' }
         );
 
         const response = await request.then(response => response.json());
@@ -42,7 +34,23 @@ export default class ActivationUtils {
         }
     }
 
-    async activateAddress(activationToken, address) {
-        
+    async activateAddress(activationToken, ethAddress, nimiqAddress) {
+        const request = fetch(
+            `${ActivationUtils.API_ROOT}/activate/address`,
+            { 
+                method: 'POST',
+                body: JSON.stringify({
+                    'activation_token': activationToken,
+                    'nimiq_address': nimiqAddress
+                }),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            }
+        );
+
+        const response = await request;
+        this.onActivateAddress(response.ok);
     }
 }
