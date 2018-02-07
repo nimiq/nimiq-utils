dea339c350f01f36a04600814ac4523be04f88f2
Don Maxito <mh@expertsinside.com>
2018-02-06 20:08:47 -0600

    error handling in activation-utils

--- activation-utils/activation-utils.js
+++ activation-utils/activation-utils.js
@@ -1,29 +1,48 @@
 export default class ActivationUtils {
     static get API_ROOT() { return 'https://activate.nimiq-network.com' }
 
+    /** @param {string} ethAddress
+     *  @return {Promise<number>}*/
     static async fetchBalance(ethAddress) {
         const response = await fetch(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xcfb98637bcae43C13323EAa1731cED2B716962fD&tag=latest&address=${ethAddress}`)
         return (await response.json()).result;
     }
 
+    /** @return {Promise<string>} */
     static async nim2ethAddress(address) {
         const hash = await Nimiq.Hash.sha256(address.serialize());
         return '0x' + Nimiq.BufferUtils.toHex(hash.subarray(0, 20));
     }
 
+    /** @param {string} friendlyAddress */
     static getUnfriendlyAddress(friendlyAddress) {
         return Nimiq.Address.fromUserFriendlyAddress(friendlyAddress);
     }
 
+    /** @param {string} dashboardToken */
     async getDashboardData(dashboardToken) {
-        const request = fetch(
-            `${ActivationUtils.API_ROOT}/list/${dashboardToken}`,
-            { method: 'GET' }
-        );
+        try {
+            const request = fetch(
+                `${ActivationUtils.API_ROOT}/list/${dashboardToken}`,
+                { method: 'GET' }
+            );
 
-        const response = await request.then(response => response.json());
-        this.onDashboardDataResult(response);
+            const result = await request.then(response => {
+                if (!response.ok) {
+                    this.onDashboardTokenError();
+                } else {
+                    return response.json();
+                }
+            });
+
+            if (result) {
+                this.onDashboardDataResult(result);
+            } 
+        } catch(e) {
+            this.onDashboardTokenError();
+        }
     }
+
 
     async isValidToken(activationToken) {
         const request = fetch(
