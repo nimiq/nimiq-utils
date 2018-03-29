150efb856adf6f25537358ca2f2c264cd988989f
danimoh <github.danimoh@neverbox.com>
2018-03-28 18:08:37 -0600

    clipboard: code style and fix demo

--- clipboard/clipboard.js
+++ clipboard/clipboard.js
@@ -1,32 +1,32 @@
 export default class Clipboard {
     static copy(text) {
         // A <span> contains the text to copy
-        var span = document.createElement('span')
-        span.textContent = text
-        span.style.whiteSpace = 'pre' // Preserve consecutive spaces and newlines
+        const span = document.createElement('span');
+        span.textContent = text;
+        span.style.whiteSpace = 'pre'; // Preserve consecutive spaces and newlines
 
         // Paint the span outside the viewport
         span.style.position = 'absolute';
         span.style.left = '-9999px';
         span.style.top = '-9999px';
 
-        var win = window
-        var selection = win.getSelection()
-        win.document.body.appendChild(span)
+        const win = window;
+        const selection = win.getSelection();
+        win.document.body.appendChild(span);
 
-        var range = win.document.createRange()
-        selection.removeAllRanges()
-        range.selectNode(span)
-        selection.addRange(range)
+        const range = win.document.createRange();
+        selection.removeAllRanges();
+        range.selectNode(span);
+        selection.addRange(range);
 
-        var success = false
+        let success = false;
         try {
-            success = win.document.execCommand('copy')
+            success = win.document.execCommand('copy');
         } catch (err) {}
 
-        selection.removeAllRanges()
-        span.remove()
+        selection.removeAllRanges();
+        span.remove();
 
-        return success
+        return success;
     }
 }
