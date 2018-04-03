bf1f5e2f976ac48af1bc5d8f4b92773fe124fd2b
Don Maxito <maxharz@gmail.com>
2018-04-03 11:35:21 -0600

    add browser check for iOS and safari

--- browser-detection/browser-detection.js
+++ browser-detection/browser-detection.js
@@ -4,4 +4,12 @@ export default class BrowserDetection {
         // see https://stackoverflow.com/a/23522755
         return /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && !/mobile/i.test(navigator.userAgent);
     }
+
+    static isSafari() {
+        return !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    }
+
+    static isIOS() {
+        return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    }
 }
