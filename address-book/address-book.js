03708222ac49d8b792c8315b52c12ae154c8c246
Sören Schwert <hello@soerenschwert.de>
2018-05-02 01:03:56 +0200

    Update nimiq.watch pool address

--- address-book/address-book.js
+++ address-book/address-book.js
@@ -5,7 +5,6 @@ export default class AddressBook {
 }
 
 AddressBook.BOOK = {
-    'NQ92 0RGS 0YGB MRY8 5MHM MBL2 HSP5 7N0U Y5C6': 'pool.nimiq.watch',
     'NQ58 U4HN TVVA FCRS VLYL 8XTL K0B7 2FVD EC6B': 'skypool',
     'NQ88 D1R3 KR4H KSY2 CQYR 5G0C 80X4 0KED 32G8': 'skypool',
     'NQ48 8CKH BA24 2VR3 N249 N8MN J5XX 74DB 5XJ8': 'skypool',
@@ -15,4 +14,5 @@ AddressBook.BOOK = {
     'NQ10 76JC KSSE 5S2R U401 NC5P M3N2 8TKQ YATP': 'pool.nimiqchain.info',
     'NQ33 DH76 PHUK J41Q LX3A U4E0 M0BM QJH9 QQL1': 'Beeppool',
     'NQ40 BHLY ND9E GAVA PMJ5 XF9Q CDDJ 5N4F L012': 'pool.bhlynd.nz',
+    'NQ90 P00L 2EG5 3SBU 7TB5 NPGG 8FNL 4JC7 A4ML': 'pool.nimiq.watch',
 }
