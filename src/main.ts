8880a418e75f061b93e4fefb91894b7982575a76
sisou <hello@soerenschwert.de>
2018-11-26 15:34:08 -0600

    Add AddressBook, InputFormat, non-default PasteHandler export

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+export * from './address-book/AddressBook';
 export * from './browser-detection/BrowserDetection';
+export { default as InputFormat } from './input-format/index.js';
+export * from './paste-handler/PasteHandler';
 export * from './utf8-tools/Utf8Tools';
 export * from './validation-utils/ValidationUtils';
-export * from './paste-handler/PasteHandler';
