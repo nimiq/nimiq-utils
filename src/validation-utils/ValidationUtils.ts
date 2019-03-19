3bd565863c5d9e251c94fb1762fb12876c0e00e7
sisou <hello@soerenschwert.de>
2019-03-19 14:15:44 +0100

    Refactor ValidationUtils

--- src/validation-utils/ValidationUtils.ts
+++ src/validation-utils/ValidationUtils.ts
@@ -13,23 +13,31 @@ export class ValidationUtils {
 
     static isUserFriendlyAddress(str: string) {
         if (!str) return;
-
         str = str.replace(/ /g, '');
+
         if (str.substr(0, 2).toUpperCase() !== 'NQ') {
-            // TODO use custom error class?
-            // @ts-ignore
-            throw new Error('Addresses start with NQ', 201);
+            throw new Error('Addresses start with NQ');
         }
+
         if (str.length !== 36) {
-            // TODO use custom error class?
-            // @ts-ignore
-            throw new Error('Addresses are 36 chars (ignoring spaces)', 202);
+            throw new Error('Addresses are 36 chars (ignoring spaces)');
         }
+
+        if (!this._alphabetCheck(str)) {
+            throw new Error('Address has invalid characters');
+        }
+
         if (this._ibanCheck(str.substr(4) + str.substr(0, 4)) !== 1) {
-            // TODO use custom error class?
-            // @ts-ignore
-            throw new Error('Address Checksum invalid', 203);
+            throw new Error('Address Checksum invalid');
         }
+    }
+
+    static _alphabetCheck(str: string) {
+        str = str.toUpperCase();
+        for (let i = 0; i < str.length; i++) {
+            if (!ValidationUtils.NIMIQ_ALPHABET.includes(str[i])) return false;
+        }
+        return true;
     }
 
     static _ibanCheck(str: string) {
@@ -53,5 +61,10 @@ export class ValidationUtils {
         } catch(e) {
             return false;
         }
+    }
+
+    static get NIMIQ_ALPHABET() {
+        // From Nimiq.BufferUtils.BASE32_ALPHABET.NIMIQ
+        return '0123456789ABCDEFGHJKLMNPQRSTUVXY';
     }
 }
