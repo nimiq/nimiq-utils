025c1443cfcd3c4d1ff63ab31208499337a29725
Daniel <github.danimoh@neverbox.com>
2019-07-11 17:31:19 +0200

    Remove utils for input-format and paste-handler
    - input-format can be used from the npm package 'input-format' directly.
      No really noteworthy changes happened in our copy in nimiq-utils. Changes
      were mostly for compatibility of native js imports in Edge. These changes
      are not relevant when using a bundler like webpack or rollup.
    - paste-handler is not in use anymore and not really necessary anymore

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,6 @@ export default {
         'build/main.js',
         'build/address-book/AddressBook.js',
         'build/browser-detection/BrowserDetection.js',
-        // 'src/input-format/InputFormat.js',
-        'build/paste-handler/PasteHandler.js',
         'build/request-link-encoding/RequestLinkEncoding.js',
         'build/utf8-tools/Utf8Tools.js',
         'build/validation-utils/ValidationUtils.js'
