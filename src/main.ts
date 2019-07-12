6f22641826c7175d1b3bd2e24db153dc58286c14
Daniel <github.danimoh@neverbox.com>
2019-07-12 22:45:21 +0200

    Clipboard: initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 export * from './address-book/AddressBook';
 export { default as BrowserDetection } from './browser-detection/BrowserDetection';
+export * from './clipboard/Clipboard';
 export * from './request-link-encoding/RequestLinkEncoding';
 export * from './utf8-tools/Utf8Tools';
 export * from './validation-utils/ValidationUtils';
