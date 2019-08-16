9c1e9e38163be7b85d73873c80afd8db11968338
Daniel <github.danimoh@neverbox.com>
2019-08-16 17:17:50 -0600

    RequestLinkEncoding: add BTC / ETH request link creation

--- src/request-link-encoding/RequestLinkEncoding.ts
+++ src/request-link-encoding/RequestLinkEncoding.ts
@@ -1,29 +1,56 @@
 import { ValidationUtils } from "../validation-utils/ValidationUtils";
 
-// Note that the encoding scheme is the same as for Safe XRouter aside route parameters (see _makeAside).
+/**
+ * @deprecated
+ */
 export function createRequestLink(
     recipient: string,
     amount?: number, // in NIM
     message?: string,
     basePath: string = window.location.host,
 ) {
+    console.warn('This method is deprecated. Use createNimiqRequestLink instead.');
+    return createNimiqRequestLink(recipient, { amount, message, basePath })
+}
+
+/**
+ * @deprecated
+ */
+export function parseRequestLink(
+    requestLink: string | URL,
+    requiredBasePath?: string,
+) {
+    console.warn('This method is deprecated. Use parseNimiqRequestLink instead.');
+    return parseNimiqRequestLink(requestLink, requiredBasePath);
+}
+
+// Note that the encoding scheme is the same as for Safe XRouter aside route parameters (see _makeAside).
+export function createNimiqRequestLink(
+    recipient: string,
+    options: {
+        amount?: number, // in NIM
+        message?: string,
+        basePath?: string,
+    } = { basePath: window.location.host },
+) {
+    let { amount, message, basePath } = options;
     if (!ValidationUtils.isValidAddress(recipient)) throw new Error(`Not a valid address: ${recipient}`);
     if (amount && typeof amount !== 'number') throw new Error(`Not a valid amount: ${amount}`);
     if (message && typeof message !== 'string') throw new Error(`Not a valid message: ${message}`);
-    const params = [
+    const optionsArray = [
         recipient.replace(/ /g, ''), // strip spaces
         amount || '',
         encodeURIComponent(message || '')
     ];
-    // don't encode empty params (if they are not followed by other non-empty params)
-    while (params[params.length - 1] === '') params.pop();
+    // don't encode empty options (if they are not followed by other non-empty options)
+    while (optionsArray[optionsArray.length - 1] === '') optionsArray.pop();
 
-    if (!basePath.endsWith('/')) basePath = `${basePath}/`;
+    if (!basePath!.endsWith('/')) basePath = `${basePath}/`;
 
-    return `${basePath}#_request/${params.join('/')}_`;
+    return `${basePath}#_request/${optionsArray.join('/')}_`;
 }
 
-export function parseRequestLink(
+export function parseNimiqRequestLink(
     requestLink: string | URL,
     requiredBasePath?: string,
 ) {
@@ -45,11 +72,11 @@ export function parseRequestLink(
     const requestRegexMatch = requestLink.hash.match(requestRegex);
     if (!requestRegexMatch) return null;
 
-    // parse params
-    const paramsSubstr = requestRegexMatch[1];
-    let [recipient, amount, message] = paramsSubstr.split('/');
+    // parse options
+    const optionsSubstr = requestRegexMatch[1];
+    let [recipient, amount, message] = optionsSubstr.split('/');
 
-    // check params
+    // check options
     recipient = recipient
         .replace(/[ +-]|%20/g, '') // strip spaces and dashes
         .replace(/(.)(?=(.{4})+$)/g, '$1 '); // reformat with spaces, forming blocks of 4 chars
@@ -70,4 +97,67 @@ export function parseRequestLink(
         parsedMessage = null;
     }
     return { recipient, amount: parsedAmount, message: parsedMessage };
+}
+
+// following BIP21
+export function createBitcoinRequestLink(
+    recipient: string,
+    options: {
+        amount?: number, // in BTC
+        fee?: number, // suggested fee in BTC, might be ignored
+        label?: string,
+        message?: string,
+    } = {},
+) {
+    if (!recipient) throw new Error('Recipient is required');
+    if (options.amount && (!Number.isFinite(options.amount) || options.amount < 0)) throw new TypeError('Invalid amount');
+    if (options.fee && (!Number.isFinite(options.fee) || options.fee < 0)) throw new TypeError('Invalid fee');
+    const query = new URLSearchParams();
+    const validQueryKeys: ['amount', 'fee', 'label', 'message'] = ['amount', 'fee', 'label', 'message'];
+    validQueryKeys.forEach((key) => {
+        const option = options[key];
+        if (!option) return;
+        // formatted value in BTC without scientific number notation
+        const btcPrecision = 8;
+        const formattedValue = typeof option === 'number'
+            ? option.toFixed(btcPrecision).replace(/\.?0*$/g, '')
+            : option.toString();
+        query.set(key, formattedValue);
+    }, '');
+    const queryString = query.toString() ? `?${query.toString()}` : ''; // also urlEncodes the values
+    return `bitcoin:${recipient}${queryString}`
+}
+
+// subset of EIP681
+export function createEthereumRequestLink(
+    recipient: string, // ETH address or ENS name
+    options: {
+        // Note that ETH values are limited to JS number precision
+        amount?: number, // in ETH
+        gasPrice?: number, // in ETH
+        gasLimit?: number, // integer in gas units, same as parameter 'gas' as specified in EIP681
+        chainId?: number,
+    } = {},
+) {
+    if (!recipient) throw new Error('Recipient is required');
+    const { amount: value, gasPrice, gasLimit, chainId } = options;
+    if (value && (!Number.isFinite(value) || value < 0)) throw new TypeError('Invalid amount');
+    if (gasPrice && (!Number.isFinite(gasPrice) || gasPrice < 0)) throw new TypeError('Invalid gasPrice');
+    if (gasLimit && (!Number.isInteger(gasLimit) || gasLimit < 0)) throw new TypeError('Invalid gasLimit');
+    const eip831Prefix = !recipient.startsWith('0x') ? 'pay-' : '';
+    const chainIdString = chainId !== undefined ? `@${chainId}` : '';
+    const query = new URLSearchParams();
+    const queryOptions: { [key: string]: number | undefined } = { value, gasLimit, gasPrice };
+    Object.keys(queryOptions).forEach((key) => {
+        const option = queryOptions[key];
+        if (!option) return;
+        // formatted ETH values in Wei with manual scientific number notation
+        const ethPrecision = 18;
+        const formattedValue = key === 'amount' || key === 'gasPrice'
+            ? option.toFixed(ethPrecision).replace(/\.?0*$/g, '') + `e${ethPrecision}`
+            : option.toString();
+        query.set(key, formattedValue);
+    }, '');
+    const queryString = query.toString() ? `?${query.toString()}` : ''; // also urlEncodes the values
+    return `ethereum:${eip831Prefix}${recipient}${chainIdString}${queryString}`;
 }
