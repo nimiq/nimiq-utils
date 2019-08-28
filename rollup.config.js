e9653a68b3ac9d7c88e1023907f6336ece40fbce
Daniel <github.danimoh@neverbox.com>
2019-08-28 11:01:46 -0600

    Tweenable

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 // rollup.config.js
 export default {
+    // list source files here to ensure they get bundled and not removed by tree-shaking.
     input: [
         'build/main.js',
         'build/address-book/AddressBook.js',
         'build/browser-detection/BrowserDetection.js',
         'build/clipboard/Clipboard.js',
         'build/request-link-encoding/RequestLinkEncoding.js',
+        'build/tweenable/Tweenable.js',
         'build/utf8-tools/Utf8Tools.js',
         'build/validation-utils/ValidationUtils.js'
     ],
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ export * from './address-book/AddressBook';
 export { default as BrowserDetection } from './browser-detection/BrowserDetection';
 export * from './clipboard/Clipboard';
 export * from './request-link-encoding/RequestLinkEncoding';
+export { default as Tweenable } from './tweenable/Tweenable';
 export * from './utf8-tools/Utf8Tools';
 export * from './validation-utils/ValidationUtils';
