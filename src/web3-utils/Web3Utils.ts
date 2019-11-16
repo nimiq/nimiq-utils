9dd141b1b6db5ea8bd3fc1f258673621f373b0d6
sisou <hello@soerenschwert.de>
2019-11-16 10:28:26 -0600

    Add semicolons

--- src/web3-utils/Web3Utils.ts
+++ src/web3-utils/Web3Utils.ts
@@ -1,5 +1,5 @@
 // this imports only the type without bundling the library
-type BigInteger = import('big-integer').BigInteger
+type BigInteger = import('big-integer').BigInteger;
 
 export type Web3Provider = {
     sendAsync<R>(options: object, callback: (error: Error, result: RpcResult<R>) => void): void,
@@ -37,47 +37,47 @@ export function getProvider() {
     // @ts-ignore ethereum and web3 do not exists on window
     if (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined') {
         // @ts-ignore ethereum and web3 do not exists on window
-        return (window.ethereum as Web3Provider) || (window.web3.currentProvider as Web3Provider)
+        return (window.ethereum as Web3Provider) || (window.web3.currentProvider as Web3Provider);
     }
 
-    throw new Error('No Web3 provider found')
+    throw new Error('No Web3 provider found');
 }
 
 export function hasProvider(): boolean {
     try {
-        getProvider()
-        return true
+        getProvider();
+        return true;
     } catch (e) {
-        return false
+        return false;
     }
 }
 
 export function getProviderName(): string {
-    const provider = getProvider()
+    const provider = getProvider();
 
-    if (provider.isMetaMask) return 'MetaMask'
-    return 'Browser Wallet'
+    if (provider.isMetaMask) return 'MetaMask';
+    return 'Browser Wallet';
 }
 
 export async function getAddress() {
-    const provider = getProvider()
-    await provider.enable()
+    const provider = getProvider();
+    await provider.enable();
 
-    if (provider.selectedAddress) return provider.selectedAddress
+    if (provider.selectedAddress) return provider.selectedAddress;
 
     // While MetaMask supports both `enable()` and `send('eth_requestAccounts')` to get active addresses,
     // Opera does not. The `eth_accounts` method works for both.
-    const address = (await _rpc<string[]>('eth_accounts'))[0]
-    return address
+    const address = (await _rpc<string[]>('eth_accounts'))[0];
+    return address;
 }
 
 export async function getNetworkVersion() {
-    const provider = getProvider()
-    if (provider.networkVersion) return parseInt(provider.networkVersion)
+    const provider = getProvider();
+    if (provider.networkVersion) return parseInt(provider.networkVersion);
 
     // While MetaMask supports the `networkVersion` prop,
     // Opera does not. The `net_version` method works for both.
-    return parseInt(await _rpc<string>('net_version'))
+    return parseInt(await _rpc<string>('net_version'));
 }
 
 export async function sendTransaction(
@@ -85,89 +85,89 @@ export async function sendTransaction(
     network: number | string = 'main',
     changeNetworkCallback?: (network: string) => void,
 ) {
-    const provider = getProvider()
-    await provider.enable()
+    const provider = getProvider();
+    await provider.enable();
 
-    let chainId = stringToChainId(params.chainId || network)
+    const chainId = stringToChainId(params.chainId || network);
 
     while (await getNetworkVersion() !== chainId) {
         // Prevent website from reloading when user switches network
-        provider.autoRefreshOnNetworkChange = false
+        provider.autoRefreshOnNetworkChange = false;
 
-        const networkName = _capitalizeString(chainIdToString(chainId))
+        const networkName = _capitalizeString(chainIdToString(chainId));
 
-        if (typeof changeNetworkCallback === 'function') changeNetworkCallback(networkName)
-        else alert(`To send this transaction, you must change the network in your Ethereum provider to "${networkName}"`)
+        if (typeof changeNetworkCallback === 'function') changeNetworkCallback(networkName);
+        else alert(`To send this transaction, you must change the network in your Ethereum provider to "${networkName}"`);
 
-        await new Promise<number>(resolve => {
-            provider.on('networkChanged', resolve)
-        })
+        await new Promise<number>((resolve) => {
+            provider.on('networkChanged', resolve);
+        });
     }
 
     if (!params.from) {
-        params.from = await getAddress()
+        params.from = await getAddress();
     }
 
     // Ethereum JSON-RPC requires numbers be given in HEX representation
-    params.value = _toHex(params.value)
-    params.gas = typeof params.gas !== 'undefined' ? _toHex(params.gas) : undefined
-    params.gasPrice = typeof params.gasPrice !== 'undefined' ? _toHex(params.gasPrice) : undefined
-    params.chainId = chainId
+    params.value = _toHex(params.value);
+    params.gas = typeof params.gas !== 'undefined' ? _toHex(params.gas) : undefined;
+    params.gasPrice = typeof params.gasPrice !== 'undefined' ? _toHex(params.gasPrice) : undefined;
+    params.chainId = chainId;
 
-    return _rpc<string>('eth_sendTransaction', params)
+    return _rpc<string>('eth_sendTransaction', params);
 }
 
 export function stringToChainId(network: number | string) {
-    if (typeof network === 'number') return network
+    if (typeof network === 'number') return network;
 
     switch (network.toLowerCase()) {
         case 'main':
         case 'mainnet':
-            return 1
+            return 1;
         case 'test':
         case 'testnet':
         case 'ropsten':
-            return 3
+            return 3;
         case 'rinkeby':
-            return 4
+            return 4;
         case 'goerli':
-            return 5
+            return 5;
         case 'kovan':
-            return 42
+            return 42;
         case 'geth':
-            return 1337
+            return 1337;
         default:
-            throw new Error('Invalid network name')
+            throw new Error('Invalid network name');
     }
 }
 
 export function chainIdToString(id: number) {
     switch (id) {
-        case 1: return 'mainnet'
-        case 3: return 'ropsten'
-        case 4: return 'rinkeby'
-        case 5: return 'goerli'
-        case 42: return 'kovan'
-        case 1337: return 'geth'
-        default: throw new Error('Invalid chain ID')
+        case 1: return 'mainnet';
+        case 3: return 'ropsten';
+        case 4: return 'rinkeby';
+        case 5: return 'goerli';
+        case 42: return 'kovan';
+        case 1337: return 'geth';
+        default: throw new Error('Invalid chain ID');
     }
 }
 
 async function _rpc<R>(method: string, params?: any) {
-    const provider = getProvider()
+    const provider = getProvider();
 
-    if (typeof params !== 'undefined' && !Array.isArray(params)) params = [params]
+    if (typeof params !== 'undefined' && !Array.isArray(params)) params = [params];
 
-    console.debug('WEB3 SEND:', method, params)
+    console.debug('WEB3 SEND:', method, params);
 
     const response = await new Promise<RpcResult<R>>(async (resolve, reject) => {
         if (typeof provider.send === 'function') {
             try {
                 // Use send() as specified in EIP-1193
-                provider.send<R>(method, params).then(resolve, reject)
+                provider.send<R>(method, params).then(resolve, reject);
             } catch (error) {
                 // Opera doesn't like EIP-1193 yet and implements a sync method with a single request object
-                try { resolve(provider.send<R>({ method, params })) } catch (error) { reject(error) }
+                try { resolve(provider.send<R>({ method, params })); } catch (error) { reject(error); }
             }
         } else if (typeof provider.sendAsync === 'function') {
             // Use deprecated sendAsync() with callback
@@ -175,20 +175,19 @@ async function _rpc<R>(method: string, params?: any) {
                 method,
                 params,
             }, (error, result) => {
-                if (error) reject(error)
-                resolve(result)
-            })
-        }
-        else reject(new Error('No known Web3 integration found'))
-    })
+                if (error) reject(error);
+                resolve(result);
+            });
+        } else reject(new Error('No known Web3 integration found'));
+    });
 
-    console.debug('WEB3 RECEIVE:', response)
+    console.debug('WEB3 RECEIVE:', response);
 
     if (response.error) {
-        throw new Error(response.error.message)
+        throw new Error(response.error.message);
     }
 
-    return response.result!
+    return response.result!;
 }
 
 function _toHex(value: string | number | bigint | BigInteger): string {
@@ -196,9 +195,9 @@ function _toHex(value: string | number | bigint | BigInteger): string {
         if (value.substring(0, 2) !== '0x') throw new Error('Non-hex-encoded strings are not yet supported');
         return value;
     }
-    return '0x' + value.toString(16);
+    return `0x${value.toString(16)}`;
 }
 
 function _capitalizeString(str: string) {
-    return str[0].toUpperCase() + str.slice(1)
+    return str[0].toUpperCase() + str.slice(1);
 }
