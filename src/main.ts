3ed6653cf25d20062a64b5aeb563101fa0ec50d4
sisou <hello@soerenschwert.de>
2020-05-29 09:51:23 +0200

    Export FiatApi in namespace, order exportes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+// ES6 Exports
 import * as AddressBook from './address-book/AddressBook'; export { AddressBook };
 import * as BrowserDetection from './browser-detection/BrowserDetection'; export { BrowserDetection }
 import * as Clipboard from './clipboard/Clipboard'; export { Clipboard };
 import * as Cookie from './cookie/Cookie'; export { Cookie };
-export * from './currency-info/CurrencyInfo';
-export * from './fiat-api/FiatApi';
-export * from './formattable-number/FormattableNumber';
+import * as FiatApi from './fiat-api/FiatApi'; export { FiatApi };
 import * as RequestLinkEncoding from './request-link-encoding/RequestLinkEncoding'; export { RequestLinkEncoding };
-export { default as Tweenable } from './tweenable/Tweenable';
 import * as Utf8Tools from './utf8-tools/Utf8Tools'; export { Utf8Tools };
 import * as ValidationUtils from './validation-utils/ValidationUtils'; export { ValidationUtils };
+
+// Classes
+export * from './currency-info/CurrencyInfo';
+export * from './formattable-number/FormattableNumber';
+export * from './tweenable/Tweenable';
