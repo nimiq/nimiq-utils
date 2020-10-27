ce7d6ba34a5899f3a74927fe75fe6e5847679e89
sisou <hello@soerenschwert.de>
2020-10-27 09:05:12 +0100

    Fix linter errors

--- tests/CurrencyInfo.spec.ts
+++ tests/CurrencyInfo.spec.ts
@@ -1,4 +1,4 @@
-/* global describe, it, expect */
+/* global describe, it */
 
 import { CurrencyInfo } from '../src/currency-info/CurrencyInfo';
 import { FiatApiSupportedFiatCurrency } from '../src/fiat-api/FiatApi';
@@ -7,7 +7,7 @@ describe('CurrencyInfo', () => {
     it('has currency symbols', () => {
         for (const code of Object.keys(FiatApiSupportedFiatCurrency)) {
             const info = new CurrencyInfo(code);
-            console.log(code, info.symbol);
+            console.log(code, info.symbol); // eslint-disable-line no-console
         }
     });
 });
