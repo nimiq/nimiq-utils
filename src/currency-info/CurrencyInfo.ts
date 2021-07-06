e71772ba3c7045fbc37f6f2a1a1f630e27bc9653
Daniel <github.danimoh@neverbox.com>
2021-07-06 17:13:53 +0200

    CurrencyInfo: add locale support

--- src/currency-info/CurrencyInfo.ts
+++ src/currency-info/CurrencyInfo.ts
@@ -30,60 +30,118 @@ export class CurrencyInfo {
         ZAR: 'R',
     };
 
-    // Regex for en-US formatted currency strings (supporting both currency symbol and currency name)
-    private static readonly NUMBER_FORMAT_REGEX = new RegExp(
-        '^'
-        + '([^\\d\\s]+)?' // currency symbol in front of number
-        + '\\s?' // Potential whitespace. en-US adds a whitespace for example in 'XYZ 1.00' but not in 'CA$1.00'
-        + '\\d+' // integer part with useGrouping: false
-        + '(?:\\D(\\d+))?' // fractional part, can be empty
-        + '(?:\\s(.+))?'// currency name after number
-        + '$',
-    );
+    // Regex for detecting the number with optional decimals in a formatted string for useGrouping: false
+    private static readonly NUMBER_REGEX = /\d+(?:\D(\d+))?/;
 
     public readonly code: string;
     public readonly symbol: string;
     public readonly name: string;
     public readonly decimals: number;
+    public readonly locale: string;
 
+    /* eslint-disable no-dupe-class-members, lines-between-class-members */
     /**
+     * This constructor is convenient when manually defining a CurrencyInfo or when no locale is to be specified.
+     * @constructor
      * @param {string} currencyCode 3-letter currency code
      * @param {number} [decimals] How many decimal positions the currency has
      * @param {string} [name] The currency's name, e.g. euros
      * @param {string} [symbol] The currency's symbol, e.g. € or CA$
      * @throws If currency code is not a well-formed currency code.
+     *//**
+     * This constructor is convenient when the CurrencyInfo's properties are to be automatically determined for a given
+     * locale.
+     * @constructor
+     * @param {string} currencyCode 3-letter currency code
+     * @param {string} locale The locale to use for auto-detecting the name and symbol
+     * @throws If currency code is not a well-formed currency code.
+     *//**
+     * This constructor is convenient when manually defining the locale or some properties of the CurrencyInfo.
+     * @constructor
+     * @param {string} currencyCode 3-letter currency code
+     * @param {Object} [options]
+     * @param {number} [options.decimals] How many decimal positions the currency has
+     * @param {string} [options.name] The currency's name, e.g. euros
+     * @param {string} [options.symbol] The currency's symbol, e.g. € or CA$
+     * @param {string} [options.locale] The locale to use when auto-detecting the name or symbol
+     * @throws If currency code is not a well-formed currency code.
      */
-    constructor(currencyCode: string, decimals?: number, name?: string, symbol?: string) {
+    constructor(currencyCode: string, decimalsOrLocaleOrOptions?: number, name?: string, symbol?: string);
+    constructor(currencyCode: string, decimalsOrLocaleOrOptions: string);
+    constructor(
+        currencyCode: string,
+        decimalsOrLocaleOrOptions?: { decimals?: number, name?: string, symbol?: string, locale?: string },
+    );
+    constructor(
+        currencyCode: string,
+        decimalsOrLocaleOrOptions?: number | string
+            | { decimals?: number, name?: string, symbol?: string, locale?: string },
+        name?: string,
+        symbol?: string,
+    ) {
+        let decimals: number | undefined;
+        let locale: string | undefined;
+
+        if (typeof decimalsOrLocaleOrOptions === 'number') {
+            decimals = decimalsOrLocaleOrOptions;
+        } else if (typeof decimalsOrLocaleOrOptions === 'string') {
+            locale = decimalsOrLocaleOrOptions;
+        } else if (typeof decimalsOrLocaleOrOptions === 'object') {
+            ({ decimals, name, symbol, locale } = decimalsOrLocaleOrOptions);
+        }
+
+        // Get the country from the currency code which is typically (but not necessarily) the first two letters,
+        // see https://en.wikipedia.org/wiki/ISO_4217#National_currencies.
+        const currencyCountry = currencyCode.substring(0, 2);
+
+        [locale] = Intl.NumberFormat.supportedLocalesOf([ // also normalizes the locales
+            ...(locale ? [locale] : []), // try user provided locale
+            currencyCountry, // test whether the country code coincides with a language code
+            `${navigator.language.substring(0, 2)}-${currencyCountry}`, // user language as spoken in currency country
+            navigator.language, // fallback
+            'en-US', // en-US as last resort
+        ]);
         const formatterOptions = {
             style: 'currency',
             currency: currencyCode, // before toUpperCase to avoid conversion of characters, e.g. Eszett to SS
             useGrouping: false,
+            numberingSystem: 'latn',
         };
-        this.code = currencyCode.toUpperCase();
+
         // Note that toLocaleString throws for not well-formatted currency codes
         // (see https://www.ecma-international.org/ecma-402/1.0/#sec-6.3.1).
         // Using regex parsing instead of NumberFormat.formatToParts which has less browser support.
-        let regexMatch = (0).toLocaleString(
-            'en-US',
+        let formattedString = (0).toLocaleString(
+            locale,
             { currencyDisplay: 'symbol', ...formatterOptions },
-        ).match(CurrencyInfo.NUMBER_FORMAT_REGEX);
-        this.decimals = decimals !== undefined
-            ? decimals
-            : regexMatch
-                ? (regexMatch[2] || '').length
-                : 2;
-        this.symbol = symbol !== undefined
-            ? symbol
-            : CurrencyInfo.EXTRA_SYMBOLS[this.code] || (regexMatch && regexMatch[1]) || this.code;
+        );
+
+        this.locale = locale;
+        this.code = currencyCode.toUpperCase();
+
+        if (decimals !== undefined) {
+            this.decimals = decimals;
+        } else {
+            const numberMatch = formattedString.match(CurrencyInfo.NUMBER_REGEX);
+            this.decimals = numberMatch ? (numberMatch[1] || '').length : 2;
+        }
+
+        if (symbol !== undefined) {
+            this.symbol = symbol;
+        } else {
+            this.symbol = CurrencyInfo.EXTRA_SYMBOLS[this.code]
+                || formattedString.replace(CurrencyInfo.NUMBER_REGEX, '').trim();
+        }
 
         if (name !== undefined) {
             this.name = name;
-            return;
+        } else {
+            formattedString = (0).toLocaleString(
+                locale,
+                { currencyDisplay: 'name', ...formatterOptions },
+            );
+            this.name = formattedString.replace(CurrencyInfo.NUMBER_REGEX, '').trim();
         }
-        regexMatch = (0).toLocaleString(
-            'en-US',
-            { currencyDisplay: 'name', ...formatterOptions },
-        ).match(CurrencyInfo.NUMBER_FORMAT_REGEX);
-        this.name = regexMatch ? regexMatch[3] || this.code : this.code;
     }
+    /* eslint-enable no-dupe-class-members, lines-between-class-members */
 }
--- tests/CurrencyInfo.spec.ts
+++ tests/CurrencyInfo.spec.ts
@@ -1,13 +1,77 @@
-/* global describe, it */
+/* global describe, it, expect */
 
 import { CurrencyInfo } from '../src/currency-info/CurrencyInfo';
 import { FiatApiSupportedFiatCurrency } from '../src/fiat-api/FiatApi';
 
 describe('CurrencyInfo', () => {
-    it('has currency symbols', () => {
+    it('can be manually created', () => {
+        const c1 = new CurrencyInfo('abc', 5, 'MyCurrency', 'X');
+        const c2 = new CurrencyInfo('abc', {
+            decimals: 5,
+            name: 'MyCurrency',
+            symbol: 'X',
+            locale: 'en-UK',
+        });
+
+        expect(c1.code).toBe('ABC');
+        expect(c1.decimals).toBe(5);
+        expect(c1.name).toBe('MyCurrency');
+        expect(c1.symbol).toBe('X');
+        expect(c1.locale).toBeDefined();
+
+        expect(c2.code).toBe('ABC');
+        expect(c2.decimals).toBe(5);
+        expect(c2.name).toBe('MyCurrency');
+        expect(c2.symbol).toBe('X');
+        expect(c2.locale).toBe('en-UK');
+    });
+
+    it('can be automatically created', () => {
+        const c1 = new CurrencyInfo('eur');
+        const c2 = new CurrencyInfo('eur', 'de');
+        const c3 = new CurrencyInfo('eur', { locale: 'de' });
+        const nameRegex = /euro/i; // generated name depends on test environment, but should include some form of euro
+
+        expect(c1.code).toBe('EUR');
+        expect(c1.decimals).toBe(2);
+        expect(c1.name).toMatch(nameRegex);
+        expect(c1.symbol).toBe('€');
+        expect(c1.locale).toBeDefined();
+
+        expect(c2.code).toBe('EUR');
+        expect(c2.decimals).toBe(2);
+        expect(c2.name).toMatch(nameRegex);
+        expect(c2.symbol).toBe('€');
+        expect(c2.locale).toBeDefined();
+
+        expect(c2).toEqual(c3);
+    });
+
+    it('has custom currency symbols', () => {
         for (const code of Object.keys(FiatApiSupportedFiatCurrency)) {
             const info = new CurrencyInfo(code);
-            console.log(code, info.symbol); // eslint-disable-line no-console
+            expect(info.symbol).toBeDefined();
+            expect(info.symbol).not.toEqual(info.code);
+            // console.log(code, info.symbol); // eslint-disable-line no-console
         }
+    });
+
+    it('can generate names for different locales', () => {
+        const possibleLanguages = [];
+        for (let char1 = 'a'.charCodeAt(0); char1 <= 'z'.charCodeAt(0); ++char1) {
+            for (let char2 = 'a'.charCodeAt(0); char2 <= 'z'.charCodeAt(0); ++char2) {
+                possibleLanguages.push(`${String.fromCharCode(char1)}${String.fromCharCode(char2)}`);
+            }
+        }
+
+        const supportedLanguages = Intl.NumberFormat.supportedLocalesOf(possibleLanguages);
+        if (supportedLanguages.length <= 1) return; // nothing to compare
+
+        const generatedNames = new Set(supportedLanguages.map((lang) => new CurrencyInfo('eur', lang).name));
+        expect(generatedNames.size).toBeGreaterThan(1);
+    });
+
+    it('uses fallback locales', () => {
+        expect(new CurrencyInfo('usd', 'xy').locale).not.toMatch(/xy/i);
     });
 });
