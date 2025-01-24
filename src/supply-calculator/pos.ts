6e41be34d7aa835a5a8b8fc7aeea9b09eb2897d5
onmax <maximogarciamtnez@gmail.com>
2025-01-24 10:51:41 +0100

    feat: added albatross supply calculator lib

---
+++ src/supply-calculator/pos.ts
@@ -0,0 +1,36 @@
+import {
+    PROOF_OF_STAKE_FORK_DATE,
+    SUPPLY_AT_PROOF_OF_STAKE_FORK_DATE,
+    SUPPLY_AT_PROOF_OF_STAKE_FORK_DATE_TESTNET,
+    SUPPLY_DECAY,
+    TOTAL_SUPPLY,
+} from '../albatross-policy/albatross-policy';
+import { powi } from './utils';
+
+export interface PosSupplyAtParams {
+    /**
+     * The network name
+     *
+     * @default 'main-albatross'
+     */
+    network?: 'main-albatross' | 'test-albatross'
+}
+
+/**
+ * Calculate the PoS supply at a given time.
+ * @param {number} timestampMs The timestamp at which to calculate the PoS supply.
+ * @returns {number} The total supply of the cryptocurrency at the given time, in NIM.
+ */
+export function posSupplyAt(timestampMs: number, { network = 'main-albatross' }: PosSupplyAtParams = {}): number {
+    const ts = timestampMs - PROOF_OF_STAKE_FORK_DATE.getTime();
+    if (ts < 0) {
+        throw new Error(
+            `Invalid timestamp: ${timestampMs}. It must be greater than ${PROOF_OF_STAKE_FORK_DATE.getTime()}.`,
+        );
+    }
+
+    const genesisSupply = network === 'test-albatross'
+        ? SUPPLY_AT_PROOF_OF_STAKE_FORK_DATE_TESTNET
+        : SUPPLY_AT_PROOF_OF_STAKE_FORK_DATE;
+    return (TOTAL_SUPPLY - ((TOTAL_SUPPLY - genesisSupply * 1e5) * powi(SUPPLY_DECAY, ts))) / 1e5;
+}
---
+++ src/supply-calculator/pow.ts
@@ -0,0 +1,67 @@
+import {
+    PROOF_OF_STAKE_FORK_BLOCK,
+    PROOF_OF_STAKE_FORK_DATE,
+    PROOF_OF_WORK_EMISSION_SPEED,
+    PROOF_OF_WORK_EMISSION_TAIL_REWARD,
+    PROOF_OF_WORK_EMISSION_TAIL_START,
+    PROOF_OF_WORK_INITIAL_SUPPLY,
+} from '../albatross-policy/albatross-policy';
+
+function _powBlockRewardAt(currentSupply: number, blockHeight: number): number {
+    if (blockHeight <= 0) return 0;
+    const remaining = PROOF_OF_WORK_INITIAL_SUPPLY - currentSupply;
+    if (blockHeight >= PROOF_OF_WORK_EMISSION_TAIL_START && remaining >= PROOF_OF_WORK_EMISSION_TAIL_REWARD) {
+        return PROOF_OF_WORK_EMISSION_TAIL_REWARD;
+    }
+    const remainder = remaining % PROOF_OF_WORK_EMISSION_SPEED;
+    return (remaining - remainder) / PROOF_OF_WORK_EMISSION_SPEED;
+}
+
+function _powSupplyAfter(initialSupply: number | undefined, blockHeight: number, startHeight = 0): number {
+    let supply = initialSupply || 0;
+    for (let i = startHeight; i <= blockHeight; ++i) {
+        supply += _powBlockRewardAt(supply, i);
+    }
+    return supply;
+}
+
+/**
+ * Extrapolate PoW block height for a certain date.
+ */
+export function powBlockHeightAt(date: Date): number {
+    return PROOF_OF_STAKE_FORK_BLOCK + ((date.getTime() - PROOF_OF_STAKE_FORK_DATE.getTime()) / (60e3));
+}
+
+const supplyCache = new Map<number, number>();
+const supplyCacheInterval = 5000; // blocks
+let supplyCacheMax = 0; // blocks
+
+/**
+ * Calculate the PoW supply at a given time.
+ * @returns {number} The total supply of the cryptocurrency at the given time, in NIM.
+ */
+export function powSupplyAfter(blockHeight: number): number {
+    // Calculate last entry in supply cache that is below blockHeight.
+    let startHeight = Math.floor(blockHeight / supplyCacheInterval) * supplyCacheInterval;
+    startHeight = Math.max(0, Math.min(startHeight, supplyCacheMax));
+
+    // Calculate respective block for the last entry of the cache and the targeted height.
+    const startI = startHeight / supplyCacheInterval;
+    const endI = Math.floor(blockHeight / supplyCacheInterval);
+
+    // The starting supply is the initial supply at the beginning and a cached value afterwards.
+    let supply = startHeight === 0 ? PROOF_OF_WORK_INITIAL_SUPPLY : supplyCache.get(startHeight);
+    // Use and update cache.
+    for (let i = startI; i < endI; ++i) {
+        startHeight = i * supplyCacheInterval;
+        // Since the cache stores the supply *before* a certain block, subtract one.
+        const endHeight = (i + 1) * supplyCacheInterval - 1;
+        supply = _powSupplyAfter(supply, endHeight, startHeight);
+        // Don't forget to add one again.
+        supplyCache.set(endHeight + 1, supply);
+        supplyCacheMax = endHeight + 1;
+    }
+
+    // Calculate remaining supply (this also adds the block reward for endI*interval).
+    return _powSupplyAfter(supply, blockHeight, endI * supplyCacheInterval) / 1e5; // Luna >> NIM
+}
